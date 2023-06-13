0e3a4927b1e8220285bdf136539c2e716a71ac30
satoshi <dev.satoshi.1016@gmail.com>
2023-06-13 23:16:48 +0900

    fix: ModelのemailFieldをEmailFieldに修正

--- server/api/models.py
+++ server/api/models.py
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-  email = models.emailField(max_length=50, unique=True)
+  email = models.EmailField(max_length=50, unique=True)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
 
